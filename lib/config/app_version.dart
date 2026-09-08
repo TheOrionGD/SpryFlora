362528dc698762ada82fca2e9e4133abad0a535f
TheOrionGD <godfreytr.prof@gmail.com>
2026-09-08 12:51:12 +0530

    chore: bump build version to 2.1.5

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -1,6 +1,6 @@
 /// SpryFlora Application Version Configuration
 class AppVersion {
-  static const String versionName = '1.2.4';
+  static const String versionName = '2.1.5';
   static const int buildNumber = 1;
   static const String fullVersion = 'v$versionName (Build $buildNumber)';
 }
